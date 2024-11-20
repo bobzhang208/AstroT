c181b0d0724dc3164c893686b4ed28eb0f6c9b54
zzb <zhangzb208@gmail.com>
2024-11-20 10:05:47 +0800

    adding termninal exit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -54,6 +54,5 @@ keymap("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
 keymap("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
 keymap("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
 keymap("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
-vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>:ToggleTerm<CR>]], { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>:ToggleTerm<CR>]], term_opts)
 
